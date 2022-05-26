5923abf58f3d865d5f1226f3b54c52512554d40d
kenny escobar <kennyescobar745@gmail.com>
2022-05-26 04:02:42 -0400

    changes

--- PI-Videogames-main/client/src/components/Home.jsx
+++ PI-Videogames-main/client/src/components/Home.jsx
@@ -1,33 +1,84 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from "react-router-dom";
-import NavBar from "./NavBar";
-import styles from './Home.css'
-import Paginate from "./Paginate";
-import Card from "./Card"
-import { getAllGames } from "../actions";
 
-export default function Home(){
-    const dispatch = useDispatch();
-    const games = useSelector(state => state.games);
-    let gamesForFilter = Object.assign({}, games);
-
-    const [keny, setKeny] = useState(true)
-
-    const [currentPage, setCurrentPage] = useState(1);    
-    const [gamesPerPage, setGamesPerPage] = useState(10); 
-    const lastGame = gamesPerPage * currentPage; 
-    const firstGame = lastGame - gamesPerPage; 
-    const currentGames = gamesForFilter.length > 0 ? gamesForFilter.slice(firstGame, lastGame) : []; 
-
-    useEffect(() => {
-        dispatch(getAllGames());
-    }, [dispatch]);
-
-    const paginate = (pageNum) => {
-        setCurrentPage(pageNum)
-    };
+    import React from "react";
+    import { Link } from "react-router-dom";
+    import NavBar from "./NavBar";
+    import styles from './Home.css'
+    import { useDispatch } from "react-redux";
+    import { useSelector } from "react-redux";
+    import { useState } from "react";
+    import { useEffect } from "react";
+    import { filterCreated, filterGenre, getAllGames, getGameName, getGenres, orderAlfa, orderRating } from "../actions";
+    import Card from './Card'
     
+    
+    export default function Home(){
+    
+        const dispatch = useDispatch()
+        const allGames = useSelector(state => state.games)
+        const genres = useSelector(state => state.genres)
+    
+    
+        const [currentPage, setCurrentPage] = useState(1);
+        const [perPage,setPerPage] = useState(10)
+        const indexLast = currentPage * perPage
+        const indexFirst = indexLast - perPage
+        const currentVg = allGames.slice(indexFirst,indexLast)
+    
+    
+        const page = (numPage)=>{
+            setCurrentPage(numPage)
+        }
+    
+        const [order, setOrder] = useState('')
+        const [ name, setName] = useState('')
+    
+        useEffect(()=>{
+            dispatch(getAllGames())
+            dispatch(getGenres())
+        },[dispatch])
+
+        const [searching, setSearching] = useState(false)
+
+        function handleSubmit(e){
+            e.preventDefault();
+            dispatch(getGameName(name))
+            setSearching(true)
+            setName("")        
+        }
+
+        const handleInputChange =(e) => {
+            e.preventDefault();
+            setName(e.target.value)
+        };
+
+        function handleClick(e) {
+            e.preventDefault();
+            window.location.reload()
+            document.getElementById('nameSelect').getElementsByTagName('option')[0].selected='selected'
+            document.getElementById('ratingSelect').getElementsByTagName('option')[0].selected='selected'
+            document.getElementById('originSelect').getElementsByTagName('option')[0].selected='selected'
+            document.getElementById('genre').getElementsByTagName('option')[0].selected='selected'
+        }
+
+        function orderAlfaHdl(e){
+            e.preventDefault();
+            dispatch(orderAlfa(e.target.value));
+            setOrder(e.target.value)
+        }
+
+        function orderRatingHdl(e){
+            e.preventDefault();
+            dispatch(orderRating(e.target.value));
+            setOrder(e.target.value)
+        }
+
+        function filterGenreHdl(e){
+            dispatch(filterGenre(e.target.value));
+        }
+
+        function filterCreatedHdl(e){
+            dispatch(filterCreated(e.target.value));
+        }
     return(
         <div className="divhomeprincipal">
             <NavBar/>
@@ -55,4 +106,4 @@ export default function Home(){
         </div>
     )
 
-}
+                }
