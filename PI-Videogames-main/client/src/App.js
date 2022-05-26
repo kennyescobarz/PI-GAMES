20a97b009ba817fc03244689fff9c93e20f9c6b3
exssamartinez <exssamartinez@serviciosnutresa.onmicrosoft.com>
2022-05-25 22:25:00 -0500

    test

--- PI-Videogames-main/client/src/App.js
+++ PI-Videogames-main/client/src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter,Route, Routes} from 'react-router-dom'
 import Landing from './components/Landing'
 import Home from './components/Home';
 
-
 function App() {
   return (
     <BrowserRouter>
